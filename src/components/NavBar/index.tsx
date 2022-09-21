bab1944bcfbf2dc913487da6287a746c5be1e499
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2022-09-20 23:55:11 -0300

    Intalacion de Font Awesome y cracion inicial de la navbar

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  } from '@fortawesome/free-solid-svg-icons'
+import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons'
+import './styles.css'
+
+function NavBar() {
+    return (
+        <>
+            <nav className="nav-bar-container">
+                <div>
+                    <h1 className="title-nav-bar">Gabriel Tettamanti Development</h1>
+                </div>
+                <div
+                    className="theme-switch"
+                >
+                    <button
+                        className="theme-key"
+                    >
+                        <FontAwesomeIcon 
+                            icon={faSun}
+                            className='switch-theme-icon actived-theme'
+                        />
+                    </button>
+                    <button
+                        className="theme-key"
+                    >
+                        <FontAwesomeIcon 
+                            icon={faMoon}
+                            className='switch-theme-icon'
+                        />
+                    </button>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default NavBar;
