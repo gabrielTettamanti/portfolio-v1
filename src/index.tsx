baa0d5d8d122c50d16fc5431afd44b0ebdc67b47
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-02-06 11:32:33 -0300

    Fixed index error

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import ReactDOM from 'react-dom/client';
 import NavBar from './components/NavBar';
