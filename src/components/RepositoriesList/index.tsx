7009479d9f17737b52dca127a35d90be56b1a762
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-03-12 20:42:19 -0300

    Implemented json in respositories list

--- src/components/RepositoriesList/index.tsx
+++ src/components/RepositoriesList/index.tsx
@@ -6,40 +6,44 @@ import Repository from "../Repository";
 
 import repositoriesData from '../../assets/data/repositoriesData'
 import experimentsData from '../../assets/data/experimentsData'
+const text = require("./../../assets/data/indexTexts.json");
 
 export default () => {
   return (
-    <section
-      className="repositories-list"
-    >
-      <h2 className="repositories-list-title" id="developments-title">Desarrollos</h2>
-      {repositoriesData.map(data => {
+    <section className="repositories-list">
+      <h2 className="repositories-list-title" id="developments-title">
+        {text.es.repositoriesListTitle}
+      </h2>
+      {repositoriesData.map((data) => {
         return (
           <Repository
-            title = {data.title}
-            description = {data.description}
-            technologies = {data.technologies}
-            url = {data.url}
-            github = {data.github}
-            desktop = {data.desktop}
-            img = {data.img}
-            key = {data.title}
+            title={data.title}
+            description={data.description}
+            technologies={data.technologies}
+            url={data.url}
+            github={data.github}
+            desktop={data.desktop}
+            img={data.img}
+            key={data.title}
           />
         );
       })}
       <div>
-      <h2 className="repositories-list-title" id="experiments-title">Experimentos y código abierto<span>Web is fun</span></h2>
-        {experimentsData.map(data => {
+        <h2 className="repositories-list-title" id="experiments-title">
+          {text.es.experimentsTitle}
+          <span>{text.es.experimentsSubtitle}</span>
+        </h2>
+        {experimentsData.map((data) => {
           return (
             <Repository
-              title = {data.title}
-              description = {data.description}
-              technologies = {data.technologies}
-              url = {data.url}
-              github = {data.github}
-              desktop = {data.desktop}
-              img = {data.img}
-              key = {data.title}
+              title={data.title}
+              description={data.description}
+              technologies={data.technologies}
+              url={data.url}
+              github={data.github}
+              desktop={data.desktop}
+              img={data.img}
+              key={data.title}
             />
           );
         })}
