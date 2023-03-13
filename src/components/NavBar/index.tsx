ea6fa1ce0311cba6264e83a3b92a8597502782dd
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-03-13 20:00:54 -0300

    Added langs switch

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -6,15 +6,22 @@ function NavBar() {
     const dayTime = useTime()
     inject();
     return (
-        <header>
-            <nav className={`nav-bar-container-${dayTime}`}>
-                <div>
-                    <h1 className="title-nav-bar">Gabriel Tettamanti</h1>
-                </div>
-                
-            </nav>
-            <div className="background-header-filling"/>
-        </header>
+      <header>
+        <nav className={`nav-bar-container-${dayTime}`}>
+          <div>
+            <h1 className="title-nav-bar">Gabriel Tettamanti</h1>
+          </div>
+          <div>
+            <button>
+              <a href='/en'>EN</a>
+            </button>
+            <button>
+              <a href='/es'>ES</a>
+            </button>
+          </div>
+        </nav>
+        <div className="background-header-filling" />
+      </header>
     );
 };
 
