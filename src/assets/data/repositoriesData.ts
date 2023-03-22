0ed1a49b1ecd91073f43dfd38ae6be8c0185acff
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-03-22 19:51:24 -0300

    Deleted expetimentsData and added repos to reposData

--- src/assets/data/repositoriesData.ts
+++ src/assets/data/repositoriesData.ts
@@ -1,5 +1,6 @@
 import jsLogo from '../../assets/images/tecnologies/languages/javascript.png'
 import tsLogo from '../../assets/images/tecnologies/languages/typescript.png'
+import pyLogo from "../../assets/images/tecnologies/languages/python.png";
 import cssLogo from '../../assets/images/tecnologies/languages/css.png'
 import htmlLogo from '../../assets/images/tecnologies/languages/html.png'
 import svelteLogo from "../../assets/images/tecnologies/frontend/svelte.png";
@@ -16,6 +17,7 @@ import GitLogo from '../../assets/images/tecnologies/tools/git.png'
 import ViteLogo from '../../assets/images/tecnologies/tools/vite.png'
 import WebpackLogo from '../../assets/images/tecnologies/tools/webpack.png'
 import SassLogo from '../../assets/images/tecnologies/tools/sass.png'
+import threeJSLogo from "../../assets/images/tecnologies/tools/threejs.png";
 
 import MyPortfolio from '../../assets/images/repos/myportfolio.png'
 import MuseoPaleontologico from '../../assets/images/repos/museopaleontologico.png'
@@ -30,6 +32,11 @@ import TatetiSolidJS from '../../assets/images/repos/tatetisolid.png'
 import freeCodeCampSim from '../../assets/images/repos/freecodecampsimulation.png'
 import MercadoLiebre from '../../assets/images/repos/mercadoliebre.png'
 import ClicksSpeedTest from "../../assets/images/repos/clickspeedtest.png";
+import HelloCube from "../../assets/images/repos/hellocube.png";
+import ClickCounter from "../../assets/images/repos/clickcounter.png";
+import CameraThreeJS from "../../assets/images/repos/camerathreejs.png";
+import CalculatorPy from "../../assets/images/repos/pythoncalculator.png";
+import VueClickCounter from "../../assets/images/repos/vueclickcounter.png";
 
 const repositoriesData = [
   {
@@ -204,6 +211,64 @@ const repositoriesData = [
     github: "https://github.com/gabrielTettamanti/mercadoLiebre",
     desktop: "https://mercadoliebre-production.up.railway.app/",
     img: MercadoLiebre,
+  },
+  {
+    title: "Vue Click Counter",
+    description:
+      "Es un contador de clicks que va reaccionando a los click que se dan a los botones sumando y restando el número en el recuadro. Es mi primer acercamiento al framework frontend VueJS.",
+    technologies: [
+      vueLogo,
+      tsLogo,
+      htmlLogo,
+      SassLogo,
+      cssLogo,
+      ViteLogo,
+      GitLogo,
+    ],
+    url: "",
+    github: "https://github.com/gabrielTettamanti/vue-click-counter",
+    desktop: "https://vue-click-counter.vercel.app/",
+    img: VueClickCounter,
+  },
+  {
+    title: "React Click Counter",
+    description:
+      "Es un contador de clicks que va sumando y restando el número del recuadro. El objetivo era aprender los nuevos hooks de React, siendo estos el useEffect y el useState.",
+    technologies: [reactLogo, jsLogo, cssLogo, htmlLogo, GitLogo, WebpackLogo],
+    url: "https://youtu.be/QRZZ-RGerLo",
+    github: "https://github.com/gabrielTettamanti/react-click-counter",
+    desktop: "https://reactclickcounter.vercel.app/",
+    img: ClickCounter,
+  },
+  {
+    title: "Calculadora con Python",
+    description:
+      "Es una calculadora desarrollada con python que cuenta con un cliente para usarse como aplicación de escritorio.",
+    technologies: [pyLogo],
+    url: "",
+    github: "https://github.com/gabrielTettamanti/python-calculator",
+    desktop: "",
+    img: CalculatorPy,
+  },
+  {
+    title: "Cámaras ThreeJS",
+    description:
+      "Es mi primera prueba con la tecnología de Cámara de ThreeJS. Desde una perspectiva alejada se muestra una cámara con un campo de visión observando un cubo y un fondo más alejado.",
+    technologies: [jsLogo, htmlLogo, threeJSLogo, GitLogo],
+    url: "",
+    github: "https://github.com/gabrielTettamanti/cameraThreeJS",
+    desktop: "https://camerathreejs.vercel.app/",
+    img: CameraThreeJS,
+  },
+  {
+    title: "Hello Cube",
+    description:
+      "Es mi primera prueba usando la librería ThreeJS de Javascript. Esta es la herramienta más usada en el desarrollo de software 3D web.",
+    technologies: [jsLogo, htmlLogo, threeJSLogo, GitLogo],
+    url: "https://youtu.be/uOqDkRhXWZY",
+    github: "https://github.com/gabrielTettamanti/helloCubeThreeJS",
+    desktop: "https://hellocube.vercel.app/",
+    img: HelloCube,
   },
 ];
 
--- src/components/RepositoriesList/index.tsx
+++ src/components/RepositoriesList/index.tsx
@@ -5,15 +5,19 @@ import "./styles.css";
 import Repository from "../Repository";
 
 import repositoriesData from '../../assets/data/repositoriesData'
-import experimentsData from '../../assets/data/experimentsData'
 const text = require("./../../assets/data/indexTexts.json");
 
 export default () => {
   let lang = sessionStorage.getItem("lang");
   return (
     <section className="repositories-list">
-      <h2 className="repositories-list-title" id="developments-title">
+      <h2 className="repositories-list-title" id="experiments-title">
         {lang === "en" ? text.en.repositoriesListTitle : text.es.repositoriesListTitle}
+        <span>
+          {lang === "en"
+            ? text.en.experimentsSubtitle
+            : text.es.experimentsSubtitle}
+        </span>
       </h2>
       {repositoriesData.map((data) => {
         return (
@@ -29,26 +33,6 @@ export default () => {
           />
         );
       })}
-      <div>
-        <h2 className="repositories-list-title" id="experiments-title">
-          {lang === "en" ? text.en.experimentsTitle : text.es.experimentsTitle}
-          <span>{lang === "en" ? text.en.experimentsSubtitle : text.es.experimentsSubtitle}</span>
-        </h2>
-        {experimentsData.map((data) => {
-          return (
-            <Repository
-              title={data.title}
-              description={data.description}
-              technologies={data.technologies}
-              url={data.url}
-              github={data.github}
-              desktop={data.desktop}
-              img={data.img}
-              key={data.title}
-            />
-          );
-        })}
-      </div>
     </section>
   );
 };
