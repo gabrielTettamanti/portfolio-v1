3b9ab151b85ec22987860700e286a06f0a1ed8f2
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-04-03 14:50:30 -0300

    Added English CV

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -9,8 +9,14 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 function Contact() {
+    const URL_CV_ES =
+        "https://drive.google.com/file/d/1uSBbpskdNQ5OAOfc6jy-LIfbZSZvLQ61/view?usp=sharing";
+    const URL_CV_EN =
+      "https://drive.google.com/file/d/1E441GTpvPRYg_DpSf2yZF8-q_fUU5yld/view?usp=sharing";
+
     const dayTime = useTime()
     let lang = sessionStorage.getItem("lang");
+
     const handleMailTo = () => {
         let toastMessage = () => {
             if (lang === 'en') {
@@ -34,31 +40,42 @@ function Contact() {
     }
 
     return (
-        <footer>
-            <ToastContainer />
-            <div className={`contact-container-${dayTime}`}>
-                <div className="contact-buttons">
-                </div>
-                <a href="#nav-menu">
-                    <FontAwesomeIcon icon={faBars} className="arrow-down-scroller" />
-                </a>
-                <address className="contact-buttons">
-                    <a href="https://drive.google.com/file/d/1Vw0-ZFCLEB5ZQtZZPLaLIWL0yj8ZShHp/view?usp=sharing" target="_blank">
-                        <p className="contact-button cv-p">Curriculum Vitae</p>
-                    </a>
-                    <a href="https://www.linkedin.com/in/gabrieltettamanti/" target="_blank">
-                        <FontAwesomeIcon icon={faLinkedin} className="contact-button contact-icon" />
-                    </a>
-                    <button className="mail-button" onClick={() => handleMailTo() }>
-                        <FontAwesomeIcon icon={faEnvelope} className="contact-button contact-icon" />
-                    </button>
-                    <a href="https://github.com/gabrielTettamanti" target="_blank">
-                        <FontAwesomeIcon icon={faGithub} className="contact-button contact-icon" />
-                    </a>
-                </address>
-            </div>
-            <div className="background-footer-filling"/>
-        </footer>
+      <footer>
+        <ToastContainer />
+        <div className={`contact-container-${dayTime}`}>
+          <div className="contact-buttons"></div>
+          <a href="#nav-menu">
+            <FontAwesomeIcon icon={faBars} className="arrow-down-scroller" />
+          </a>
+          <address className="contact-buttons">
+            <a href={lang === "en" ? URL_CV_EN : URL_CV_ES} target="_blank">
+              <p className="contact-button cv-p">Curriculum Vitae</p>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/gabrieltettamanti/"
+              target="_blank"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                className="contact-button contact-icon"
+              />
+            </a>
+            <button className="mail-button" onClick={() => handleMailTo()}>
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className="contact-button contact-icon"
+              />
+            </button>
+            <a href="https://github.com/gabrielTettamanti" target="_blank">
+              <FontAwesomeIcon
+                icon={faGithub}
+                className="contact-button contact-icon"
+              />
+            </a>
+          </address>
+        </div>
+        <div className="background-footer-filling" />
+      </footer>
     );
 };
 
